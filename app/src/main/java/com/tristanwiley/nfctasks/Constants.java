5a5e84ced3a94ac53b6b5cd1b90bb31969452805
AdamMc331 <amcneilly331@gmail.com>
2016-02-20 10:51:46 -0500

    I can read from the thermostat, and houndify sucks.

--- app/src/main/java/com/tristanwiley/nfctasks/Constants.java
+++ app/src/main/java/com/tristanwiley/nfctasks/Constants.java
@@ -6,7 +6,9 @@ package com.tristanwiley.nfctasks;
  * Created by adammcneilly on 2/20/16.
  */
 public class Constants {
-    public static final String CLIENT_ID = "24b7b00f-2695-46a9-974f-56a91c8071ea";
-    public static final String CLIENT_SECRET = "lbIvaQPGiNHDapkT2vL2VbqQ8";
+    public static final String NEST_CLIENT_ID = "24b7b00f-2695-46a9-974f-56a91c8071ea";
+    public static final String NEST_CLIENT_SECRET = "lbIvaQPGiNHDapkT2vL2VbqQ8";
+    public static final String HOUND_CLIENT_ID = "r0XoKfO_XbYQ4WvpBVycxg==";
+    public static final String HOUND_CLIENT_SECRET = "18a0yRkZnI5AQ0dgQNZU9Sf1GxkfDy18hpIISVbmfu_kXx96E0viQgIEdnymCsBekUled72WrEd6bPmCul8DsQ==";
     public static final String REDIRECT_URL = "https://androidessence.com";
 }
--- app/src/main/java/com/tristanwiley/nfctasks/NestActivity.java
+++ app/src/main/java/com/tristanwiley/nfctasks/NestActivity.java
@@ -1,23 +1,36 @@
 package com.tristanwiley.nfctasks;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.speech.tts.TextToSpeech;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.hound.android.fd.HoundSearchResult;
+import com.hound.android.fd.Houndify;
+import com.hound.android.libphs.PhraseSpotterReader;
+import com.hound.android.sdk.VoiceSearchInfo;
+import com.hound.android.sdk.audio.SimpleAudioByteStreamSource;
+import com.hound.core.model.sdk.CommandResult;
+import com.hound.core.model.sdk.HoundResponse;
 import com.nestlabs.sdk.GlobalUpdate;
 import com.nestlabs.sdk.NestAPI;
 import com.nestlabs.sdk.NestException;
 import com.nestlabs.sdk.NestListener;
 import com.nestlabs.sdk.NestToken;
-import com.nestlabs.sdk.Structure;
 import com.nestlabs.sdk.Thermostat;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Locale;
 
 public class NestActivity extends AppCompatActivity {
     private static final String TAG = NestActivity.class.getSimpleName();
@@ -26,7 +39,10 @@ public class NestActivity extends AppCompatActivity {
     private NestAPI mNest;
     private NestToken mToken;
     private Thermostat mThermostat;
-    private Structure mStructure;
+    private TextView mTargetTemperature;
+    private PhraseSpotterReader phraseSpotterReader;
+    private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+    private TextToSpeechMgr textToSpeechMgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +51,8 @@ public class NestActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mTargetTemperature = (TextView) findViewById(R.id.target_temperature);
+
         NestAPI.setAndroidContext(this);
         mNest = NestAPI.getInstance();
         mToken = Settings.loadAuthToken(this);
@@ -42,7 +60,7 @@ public class NestActivity extends AppCompatActivity {
         if (mToken != null) {
             authenticate(mToken);
         } else {
-            mNest.setConfig(Constants.CLIENT_ID, Constants.CLIENT_SECRET, Constants.REDIRECT_URL);
+            mNest.setConfig(Constants.NEST_CLIENT_ID, Constants.NEST_CLIENT_SECRET, Constants.REDIRECT_URL);
             mNest.launchAuthFlow(this, AUTH_TOKEN_REQUEST_CODE);
         }
 
@@ -57,10 +75,20 @@ public class NestActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(mThermostat != null) {
                     Log.v(TAG, mThermostat.toString());
-                    ((TextView)findViewById(R.id.textView)).setText(mThermostat.toString());
+                    String thermostatID = mThermostat.getDeviceId();
+                    mNest.thermostats.setHVACMode(thermostatID, "heat");
+                    long temp = mThermostat.getTargetTemperatureF();
+                    mNest.thermostats.setTargetTemperatureF(thermostatID, (temp - 1));
                 }
             }
         });
+
+        // Normally you'd only have to do this once in your Application#onCreate
+        Houndify.get(this).setClientId( Constants.HOUND_CLIENT_ID );
+        Houndify.get(this).setClientKey( Constants.HOUND_CLIENT_SECRET );
+        Houndify.get(this).setRequestInfoFactory(StatefulRequestInfoFactory.get(this));
+
+        textToSpeechMgr = new TextToSpeechMgr( this );
     }
 
     @Override
@@ -71,7 +99,18 @@ public class NestActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        if (resultCode != RESULT_OK || requestCode != AUTH_TOKEN_REQUEST_CODE) {
+        if (requestCode == Houndify.REQUEST_CODE) {
+            final HoundSearchResult result = Houndify.get(this).fromActivityResult(resultCode, intent);
+
+            if (result.hasResult()) {
+                onResponse( result.getResponse() );
+            } else if (result.getErrorType() != null) {
+                onError(result.getException(), result.getErrorType());
+            } else {
+                //TODO:
+                // textView.setText("Aborted search");
+            }
+        } else if (resultCode != RESULT_OK || requestCode != AUTH_TOKEN_REQUEST_CODE) {
             Log.e(TAG, "Finished with no result.");
             return;
         }
@@ -94,6 +133,7 @@ public class NestActivity extends AppCompatActivity {
             public void onUpdate(@NonNull GlobalUpdate update) {
                 // Get first thermostat
                 mThermostat = update.getThermostats().get(0);
+                mTargetTemperature.setText(String.valueOf(mThermostat.getTargetTemperatureF()));
             }
         });
     }
@@ -126,5 +166,179 @@ public class NestActivity extends AppCompatActivity {
                 // mNest.launchAuthFlow(mActivity, AUTH_TOKEN_REQUEST_CODE);
             }
         });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startPhraseSpotting();
+    }
+
+    /**
+     * Called to start the Phrase Spotter
+     */
+    private void startPhraseSpotting() {
+        if ( phraseSpotterReader == null ) {
+            phraseSpotterReader = new PhraseSpotterReader(new SimpleAudioByteStreamSource());
+            phraseSpotterReader.setListener( phraseSpotterListener );
+            phraseSpotterReader.start();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // if we don't, we must still be listening for "ok hound" so teardown the phrase spotter
+        if ( phraseSpotterReader != null ) {
+            stopPhraseSpotting();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // if we don't, we must still be listening for "ok hound" so teardown the phrase spotter
+        if ( textToSpeechMgr != null ) {
+            textToSpeechMgr.shutdown();
+            textToSpeechMgr = null;
+        }
+    }
+
+    /**
+     * Called to stop the Phrase Spotter
+     */
+    private void stopPhraseSpotting() {
+        if ( phraseSpotterReader != null ) {
+            phraseSpotterReader.stop();
+            phraseSpotterReader = null;
+        }
+    }
+
+    /**
+     * Implementation of the PhraseSpotterReader.Listener interface used to handle PhraseSpotter
+     * call back.
+     */
+    private final PhraseSpotterReader.Listener phraseSpotterListener = new PhraseSpotterReader.Listener() {
+        @Override
+        public void onPhraseSpotted() {
+
+            // It's important to note that when the phrase spotter detects "Ok Hound" it closes
+            // the input stream it was provided.
+            mainThreadHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    stopPhraseSpotting();
+                    // Now start the HoundifyVoiceSearchActivity to begin the search.
+                    Houndify.get( NestActivity.this ).voiceSearch( NestActivity.this );
+                }
+            });
+        }
+
+        @Override
+        public void onError(final Exception ex) {
+
+            // for this sample we don't care about errors from the "Ok Hound" phrase spotter.
+
+        }
+    };
+
+    /**
+     * Called from onActivityResult() above
+     *
+     * @param response
+     */
+    private void onResponse(final HoundResponse response) {
+        if (response.getResults().size() > 0) {
+            // Required for conversational support
+            StatefulRequestInfoFactory.get(this).setConversationState(response.getResults().get(0).getConversationState());
+
+            //TODO:
+            // textView.setText("Received response\n\n" + response.getResults().get(0).getWrittenResponse());
+            textToSpeechMgr.speak(response.getResults().get(0).getSpokenResponse());
+
+            /**
+             * "Client Match" demo code.
+             *
+             * Houndify client apps can specify their own custom phrases which they want matched using
+             * the "Client Match" feature. This section of code demonstrates how to handle
+             * a "Client Match phrase".  To enable this demo first open the
+             * StatefulRequestInfoFactory.java file in this project and and uncomment the
+             * "Client Match" demo code there.
+             *
+             * Example for parsing "Client Match"
+             */
+            if ( response.getResults().size() > 0 ) {
+                CommandResult commandResult = response.getResults().get( 0 );
+                if ( commandResult.getCommandKind().equals("ClientMatchCommand")) {
+                    JsonNode matchedItemNode = commandResult.getJsonNode().findValue("MatchedItem");
+                    String intentValue = matchedItemNode.findValue( "Intent").textValue();
+
+                    if ( intentValue.equals("GIVE_TEMP") ) {
+                        textToSpeechMgr.speak("The target temperature is " + String.valueOf(mThermostat.getTargetTemperatureF()) + " degress fahrenheit.");
+                    }
+                }
+            }
+        }
+        else {
+            //TODO:
+            // textView.setText("Received empty response!");
+        }
+    }
+
+    /**
+     * Called from onActivityResult() above
+     *
+     * @param ex
+     * @param errorType
+     */
+    private void onError(final Exception ex, final VoiceSearchInfo.ErrorType errorType) {
+        //TODO:
+        // textView.setText(errorType.name() + "\n\n" + exceptionToString(ex));
+    }
+
+    private static String exceptionToString(final Exception ex) {
+        try {
+            final StringWriter sw = new StringWriter(1024);
+            final PrintWriter pw = new PrintWriter(sw);
+            ex.printStackTrace(pw);
+            pw.close();
+            return sw.toString();
+        }
+        catch (final Exception e) {
+            return "";
+        }
+    }
+
+
+    /**
+     * Helper class used for managing the TextToSpeech engine
+     */
+    class TextToSpeechMgr implements TextToSpeech.OnInitListener {
+        private TextToSpeech textToSpeech;
+
+        public TextToSpeechMgr( Activity activity ) {
+            textToSpeech = new TextToSpeech( activity, this );
+        }
+
+        @Override
+        public void onInit( int status ) {
+            // Set language to use for playing text
+            if ( status == TextToSpeech.SUCCESS ) {
+                int result = textToSpeech.setLanguage(Locale.US);
+            }
+        }
+
+        public void shutdown() {
+            textToSpeech.shutdown();
+        }
+
+        /**
+         * Play the text to the device speaker
+         *
+         * @param textToSpeak
+         */
+        public void speak( String textToSpeak ) {
+            textToSpeech.speak(textToSpeak, TextToSpeech.QUEUE_ADD, null);
+        }
     }
 }
